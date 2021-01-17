85213af561fd8520434097d6caf5397f4adbe3e0
bkc-tomi <bkc.tomi1979@gamil.com>
2021-01-17 17:06:55 +0900

    汎用ビューの適用

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('',                        views.IndexView.as_view(),   name='index'),
     path('<int:pk>/',               views.DetailView.as_view(),  name='detail'),
-    path('<int:pk>/result/',       views.ResultsView.as_view(), name='result'),
+    path('<int:pk>/result/',        views.ResultsView.as_view(), name='result'),
     path('<int:question_id>/vote/', views.vote,                  name='vote'),
 ]
